2c62a8fdb209f91c49112bf25730c3e51ae5774d
Connor <71336850+cak1163@users.noreply.github.com>
2025-04-18 14:22:51 -0400

    Add files via upload

---
+++ python code/morse_generator_and_decoder.py
@@ -0,0 +1,101 @@
+# Morse Code Generator and Decoder
+# Embedded Python Block (gr.sync_block)
+
+import numpy as np
+from gnuradio import gr
+
+class blk(gr.sync_block):
+    def __init__(self):
+        gr.sync_block.__init__(
+            self,
+            name='Morse Generator and Decoder',
+            in_sig=[np.float32],
+            out_sig=[]
+        )
+	# Morse code output
+        self.morse_output = ''
+
+	# Morse code translation
+        self.letter = ''
+        self.morse_phrase = ''
+
+	# Morse code dictionary
+        self.morse_code = {
+            '.-': 'A', '-...': 'B', '-.-.': 'C', '-..': 'D', '.': 'E',
+            '..-.': 'F', '--.': 'G', '....': 'H', '..': 'I', '.---': 'J',
+            '-.-': 'K', '.-..': 'L', '--': 'M', '-.': 'N', '---': 'O',
+            '.--.': 'P', '--.-': 'Q', '.-.': 'R', '...': 'S', '-': 'T',
+            '..-': 'U', '...-': 'V', '.--': 'W', '-..-': 'X', '-.--': 'Y',
+            '--..': 'Z','-----': '0', '.----': '1', '..---': '2', '...--': '3',
+            '....-': '4', '.....': '5', '-....': '6', '--...': '7',
+            '---..': '8', '----.': '9'
+        }
+
+        self.last_bit = None # Store previous bit for comparison
+        self.counter = 0 # Tracks bit duration
+        self.samp_rate = 100000 # Signal sample rate
+        self.samples_per_ms = self.samp_rate / 1000 # Convert sample rate from seconds to miliseconds
+
+
+    def decode(self, code):
+        if code != '':
+            letter = self.morse_code.get(code, '?') # '?' if not in Morse code dictionary
+            self.morse_phrase = self.morse_phrase + f"{code} " # Appending Morse code 
+            self.letter = self.letter + letter
+
+    def work(self, input_items, output_items):
+	# Threshold to determine binary "1" for input values > 0.1V
+
+        bits = (input_items[0] > 0.1).astype(np.int8)
+
+	# Threshold and duration conditions for all bits
+        for i in bits:
+
+	    # Initialization for first sample
+            if self.last_bit is None:
+                self.last_bit = i # Store for comparison
+                self.counter = 1 # Initialize duration of bit
+
+            elif i == self.last_bit:
+		# Increment counter if current bit is the same as previous
+
+                self.counter = self.counter + 1
+		# Changed bit, determine duration period in miliseconds
+            else:
+                duration = self.counter / self.samples_per_ms
+		
+		# Morse code characterization from duration
+                if self.last_bit == 1:
+                    if duration <= 1:
+                        self.morse_output = self.morse_output + '.'
+                    elif duration > 1:
+                        self.morse_output = self.morse_output + '-'
+		
+		# Previous bit is 0, threshold conditions
+                else:
+                    if 1 < duration <= 2:
+                        self.decode(self.morse_output)
+                        self.morse_output = ''
+                    elif 2 < duration <= 3:
+                        self.decode(self.morse_output)
+                        self.morse_output = ''
+                        self.letter = self.letter + ' '
+                        self.morse_phrase = self.morse_phrase + '  '
+                    
+		# Other shorter or longer pauses do nothing
+
+                self.last_bit = i
+                self.counter = 1
+
+        return len(input_items[0])
+
+    def stop(self):
+	# Output information at the end of run
+
+        self.decode(self.morse_output)
+        if self.letter or self.morse_phrase != '':
+	    
+	    # Decoded message in Morse code and ASCII letters
+            print("Transmitted Morse:  ", self.morse_phrase)
+            print("Transmitted Phrase:  ", self.letter)
+        return True
